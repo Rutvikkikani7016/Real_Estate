6825773bdcc0c74f4c2f79f92da199860d3e4633
rutvikkikani <rutvikkikani36@gmail.com>
2023-11-27 18:35:53 +0530

    fix list item bug

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -60,16 +60,15 @@ export default function Listing() {
         <div>
           <Swiper navigation>
             {listing.imageUrls.map((url) => (
-            <SwiperSlide key={url}>
-            <div
-              className="h-[550px] w-full" // Set width to 100% and height to 550px
-              style={{
-                background: `url(${url}) center no-repeat`,
-                backgroundSize: "cover",
-              }}
-            ></div>
-          </SwiperSlide>
-          
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[550px] w-full" // Set width to 100% and height to 550px
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
             ))}
           </Swiper>
           <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
